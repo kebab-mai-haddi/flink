09698a1f1a225afb1e5f725e0d8e4b237207f149
Aviral Srivastava <avsrivas@redhat.com>
2020-07-26 00:18:43 -0400

    adds fraud detection package: wip

---
+++ flink-examples/flink-examples-streaming/src/main/java/org/apache/flink/streaming/examples/spendreport/FraudDetectionAvi.java
@@ -0,0 +1,34 @@
+package org.apache.flink.streaming.examples.spendreport;
+
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.
+import org.apache.flink.walkthrough.common.entity.Alert;
+import org.apache.flink.walkthrough.common.entity.Transaction;
+import org.apache.flink.walkthrough.common.sink.AlertSink;
+import org.apache.flink.walkthrough.common.source.TransactionSource;
+
+/**
+ * Normal hoes.
+ */
+public class FraudDetectionAvi {
+
+	public static void main(String[] args) throws Exception {
+		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+		DataStream<Transaction> transactions = env
+			.addSource(new TransactionSource())
+			.name("transactions");
+
+		DataStream<Alert> alerts = transactions
+			.keyBy(Transaction::getAccountId)
+			.process(new FraudDetectorAvi())
+			.name("fraud-detector");
+
+		alerts
+			.addSink(new AlertSink())
+			.name("send-alerts");
+
+		env.execute("Fraud Detection");
+	}
+}
